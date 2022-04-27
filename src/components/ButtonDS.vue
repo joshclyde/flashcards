e65fa509473e00178fb27cb2e8ad2081d31073f3
Josh Clyde <joshclyde11@gmail.com>
2022-04-27 09:56:40 -0400

    Refactor button styles to be a global class
    I need to make a new component for a form's submit button and
    want to reuse the styles I have already written for the regular
    button component. To share the styles, I put the button styles
    in a global class so that multiple components can use that class.

--- src/components/ButtonDS.vue
+++ src/components/ButtonDS.vue
@@ -3,39 +3,7 @@ const props = defineProps<{ onClick: () => void; className?: string }>();
 </script>
 
 <template>
-  <button :class="props.className" @click="props.onClick"><slot /></button>
+  <button :class="props.className" class="button-common" @click="props.onClick">
+    <slot />
+  </button>
 </template>
-
-<style scoped>
-button {
-  color: var(--black-theme-black);
-  background-color: var(--black-theme-white);
-  padding: 0 16px;
-  height: 32px;
-  border: none;
-  border-radius: 16px;
-  cursor: pointer;
-
-  /* Border was increasing the size of the element, so switched to box-shadow. */
-  /* TODO: When making secondary button, use this. */
-  /* -webkit-box-shadow: inset 0px 0px 0px 1px var(--primary-yellow);
-  -moz-box-shadow: inset 0px 0px 0px 1px var(--primary-yellow);
-  box-shadow: inset 0px 0px 0px 1px var(--primary-yellow); */
-}
-
-button:active,
-button:hover {
-  /* Turning off the box-shadow because the button will now be filled with a color. */
-  -webkit-box-shadow: none;
-  -moz-box-shadow: none;
-  box-shadow: none;
-}
-
-button:hover {
-  background-color: var(--black-theme-secondary-white);
-}
-
-button:active {
-  background-color: var(--black-theme-tertiary-white);
-}
-</style>
